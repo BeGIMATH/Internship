a94c0a6c74d47d47e28be35ac418778010cf3f29
Begatim Bytyqi <bagatim.bytyqi@etu.univ-grenoble-alpes.fr>
2020-04-08 01:19:13 +0200

    Some small changes

--- Task_1/main_thread.cpp
+++ Task_1/main_thread.cpp
@@ -1,131 +1,101 @@
 	
-#define BOOST_THREAD_PROVIDES_FUTURE
-#include <boost/thread.hpp>
-#include <boost/thread/future.hpp>
-#include <utility>
-#include <iostream>
+#include <iostream>                   // for std::cout
+#include <boost/cstdint.hpp>      // for boost::boost::uint64_t
 #include <boost/chrono.hpp>           // for boost::chrono::high_resolution_clock
 #include <boost/thread.hpp>           // for boost::thread and boost::mutex
 #include <vector>                 // for std::vector
 #include <cassert>                    // for assert
 #include <boost/python.hpp>
-#include <Python.h>
 #include <fstream>
-const int threads_to_use = 4;
-std::vector<boost::python::list *> list_part;
+
+int threads_to_use = 4;
+
 using namespace boost::python;
 
-boost::mutex   _pyMutex; 
+const char * code = "def f(x):\n"
+             "    return x + 1\n"
+             "\n";
 
-
-void partial_change(list LOCAL_L,list l, list *final, int start_it, int chunk_size,int id)
-{
-    
-    
-    PyGILState_STATE  gstate;
-   
-    gstate = PyGILState_Ensure(); 
-    
-    object main_module = import("__main__");
-	  object main_namespace = main_module.attr("__dict__");
-     
-    PyGILState_Release(gstate);
-    
-    //PyGILState_STATE  gstate;
-    gstate = PyGILState_Ensure(); 
-    exec("def f(x):\n"
-       "  return x + 1\n"
-       "\n", main_namespace);
-         int il;
-    
-    PyObject* f = object(main_module.attr("f")).ptr();
-        
-        
-    
-    PyGILState_Release(gstate);
-    gstate = PyGILState_Ensure();
-    for (int j = 0; j < 100 ; ++j)
-        {
-          
-          il  = extract<int>(l[j + chunk_size*id]);
-          LOCAL_L.append(j);
-          
-        }
-    PyGILState_Release(gstate);
-    
-    /*
-    gstate = PyGILState_Ensure(); 
-
-    list local_l;   
-    for (int j = 0; j < 100 ; ++j)
-    {
-      il  = extract<int>(l[j + chunk_size*id]);
-      local_l.append(j);
-    }
-    PyGILState_Release(gstate);
-    
-    exec("def f(x):\n"
-       "  return x + 1\n"
-       "\n", main_namespace);
-    PyObject* f = object(main_module.attr("f")).ptr();
-   
-   PyGILState_Release(gstate);
-    
-    int el;
-
-    //_pyMutex.lock();
-
-    for (int j = 0; j < 100; ++j)
-        {
-           for (int i = 0; i < len(local_l); i++)
-            {
-                
-              
-                gstate = PyGILState_Ensure(); 
-              
-                el  = extract<int>(local_l[i]);
-                local_l[i] = call<int>(f,el);
-                PyGILState_Release(gstate);
-
-            }
-        }
-        
-        gstate = PyGILState_Ensure(); 
-        
-        *final += local_l;  
-        PyGILState_Release(gstate);*/
-           
+object initialize_function(const char * mcode = code) {
+    // object main_module = import("__main__");
+    dict main_namespace; // = main_module.attr("__dict__");
+    exec(mcode, main_namespace);
+    return main_namespace["f"];
 }
 
+using namespace boost::python;
+
+
+
+void partial_change(int start_it, int chunk_size,list l)
+{
+    printf("launched %i\n", start_it);
+
+    PyGILState_STATE gstate;
+    gstate = PyGILState_Ensure();    
+    object f = initialize_function();
+    printf("end init %i\n", start_it);
+
+    int el;
+    if (f != object()){
+        PyGILState_Release(gstate);
+        printf("ready for loop %i\n", start_it);
+        
+        gstate = PyGILState_Ensure();
+        for (int j = 0; j < 10000; ++j)
+        {
+            
+            for (int i = start_it; i < start_it + chunk_size; i++)
+            {
+                el  = extract<int>(l[i]);
+                l[i] = call<int>(f.ptr(),el);
+            }
+            
+        }
+        PyGILState_Release(gstate);
+    }
+    else 
+    {
+        PyGILState_Release(gstate);
+        printf("abort loop %i\n", start_it);        
+    }
+
+}
 int main()
 {
+  /*
+  printf("nb param : %i\n", argc);
+  if(argc >= 2){
+        threads_to_use = atoi(argv[1]);
+        printf("Nb of threads : %i\n", threads_to_use);
+  }
+  */
+  /*
   PyEval_InitThreads();
   Py_Initialize();
-  PyThreadState* m_thread_state;
+ 
   boost::chrono::high_resolution_clock::time_point start = boost::chrono::high_resolution_clock::now();
-  list_part.clear();
+ 
   list mlist;
-  list local_l;
-  for (int i = 0; i < 100; i++)
+  
+  for (int i = 0; i < 10000; i++)
   {
-    mlist.append(i);
+    mlist.append(boost::python::object(i));
   }
-  for (int i = 0; i < threads_to_use; i++){
-    list_part.push_back(new list);
-  }
+ 
   std::vector<boost::thread *> t;
   
   int list_per_thread = int(len(mlist) / threads_to_use);
-  m_thread_state = PyEval_SaveThread();
+  Py_BEGIN_ALLOW_THREADS
   for (int start_it = 0, i = 0; start_it < len(mlist); start_it += list_per_thread, i++)
   {
-    // Lump extra bits onto last thread if work items is not equally divisible by number of threads
+    
     int work_to_do = list_per_thread;
  
     if (start_it + list_per_thread < len(mlist) && start_it + list_per_thread * 2 > len(mlist))
         work_to_do = len(mlist) - start_it;
  
-    t.push_back(new boost::thread(partial_change,local_l,mlist, list_part[i], start_it, work_to_do, i));
+    t.push_back(new boost::thread(partial_change,mlist,start_it, work_to_do, i));
      
     if (work_to_do != list_per_thread)
         break;
@@ -135,11 +105,12 @@ int main()
     t[i]->join();
     delete t[i]; 
   }
-  PyEval_RestoreThread(m_thread_state);
+  Py_END_ALLOW_THREADS
   list global_l;
+  /*
   for (std::vector<list *>::iterator it = list_part.begin(); it != list_part.end(); ++it)
       global_l += **it;
-  /*
+  
   std::ofstream myfile;
         myfile.open("example.txt");
         
@@ -150,6 +121,8 @@ int main()
         
         myfile.close();
   */
+  /*
   boost::chrono::high_resolution_clock::time_point end = boost::chrono::high_resolution_clock::now();
   std::cout << "Time taken: " << (end - start).count() * ((double) boost::chrono::high_resolution_clock::period::num / boost::chrono::high_resolution_clock::period::den) << std::endl;
+  */
 }
