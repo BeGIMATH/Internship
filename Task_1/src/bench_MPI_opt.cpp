e2536353f767bba9aff75b622493f23f23e0edfe
Begatim Bytyqi <bagatim.bytyqi@etu.univ-grenoble-alpes.fr>
2020-04-14 20:47:19 +0200

    Final Version

---
+++ Task_1/src/bench_MPI_opt.cpp
@@ -0,0 +1,16 @@
+#include "tools.h"
+int main()
+{
+    MPI_Init(NULL, NULL);
+    int list_lengths[3] = {1000,5000,10000};
+    int Len = sizeof(list_lengths)/sizeof(list_lengths[0]);
+    
+    for(int i = 0; i < Len; i++)
+    {
+        pure_mpi_function_opt(list_lengths[i]);
+        
+    }
+    
+    MPI_Finalize();
+    return 0;
+}
