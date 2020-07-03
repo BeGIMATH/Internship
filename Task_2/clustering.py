2d81e7b4a865efd9fa8e33422a40275171815803
bytyqib <Bagatim.Bytyqi@etu.univ-grenoble-alpes.fr>
2020-07-03 16:23:25 +0200

    Small changes

--- Task_2/clustering.py
+++ Task_2/clustering.py
@@ -165,40 +165,43 @@ def SFC_BF(T,p,alpha):
     #Define the size of the cluster
     c = int(len(T)/p)
 
-    def BF(remain,first_time,Q):
+    def BF(remain,first_time,Q,last_cluster):
         #Add into the queue only subtrees of size that have not been added before
         sub = None
         
-        if first_time:
+        if last_cluster:
+                print("Last cluster assign the left nodes")
+                sub = 0
+                print("sub")
+        
+        
+        elif first_time:
             print("First-time")
             
             Nodes = list(post_order(T,T.root))
-
-            print("Nodes left inside the tree", len(Nodes))
+            
             #Fill the queue by traversing the tree and finding the subtrees of size less than remain
             for v in Nodes:
                 #Check if it is maxima else set it as a maximum
                 if T.parent(v):
                     if weight[v] <= remain and weight[T.parent(v)] > remain:
                         Q.append(v)
-                        print("Weight of the subtree added",weight[v])
-                        
                         #Check if it is optimal
-                        if weight[v] <= remain and weight[v] + 1 > remain:
-                            print("Found optimal subtree")
+                        if weight[v] <= remain and weight[v] + 1 > remain:    
                             Q.append(v) 
                             break
-            if len(Nodes) <= c:
-                sub = 0   
+        
+
             if Q.size() > 0:
                 sub = Q.pop()
+        
         else:
             print("Second-time")
             
             #Take a subtree from the queue and check if it has a mximal subtree
-            sub_tree_not_found = True 
-            sub_tree_size = 0
-            while sub_tree_not_found and Q.size() > 0:
+            
+            max_tree = 0
+            while Q.size() > 0:
                 
                 node = Q.pop()
             
@@ -207,52 +210,58 @@ def SFC_BF(T,p,alpha):
                 for v in local_nodes:
                     if T.parent(v):
                         #Find a potential maximal subtree
-                        if weight[v] <= remain and weight[T.parent(v)] > remain and weight[v] > sub_tree_size:
-                            sub_tree_size = weight[v]
+                        if weight[v] <= remain and weight[T.parent(v)] > remain and weight[v] > max_tree:
+                            max_tree = weight[v]
                             sub = v
-                        
+
                         #Check if it is optimal
                         if weight[v] <= remain and weight[v] + 1 > remain:
-                            sub = v 
-                            sub_tree_not_found = False
                             
-            
+                            sub = v 
+                            break
         remove_weight = weight[sub]
         
         for w in list(ancestors(T,sub)):
             weight[w] = weight[w] - remove_weight
         #Remove the founded subtree
-        sub_tree_found = list(pre_order(my_mtg,sub))
-        print("Length of the founded subtree",len(sub_tree_found))
+        sub_tree_found = list(pre_order(T,sub))
+       
         if sub != T.root:
             for vtx_id in sub_tree_found:
                 #Remove the vertices from the weights array and also from the tree      
                 T.remove_tree(vtx_id)
+        elif sub==0:
+            sub_tree_found = list(pre_order(T,sub))
+
         return sub_tree_found
         
     remain = 0
     
     for i in range(p):
-        #For each cluster initializa the queue from the begining
+        #For each cluster initialize the queue from the begining
         Qu = Priority_queue(weight)
        
         target = c
         remain = target 
         first_time = True
         print("cluster",i)
-        
+        last_cluster = False
         while len(C[i]) <= (1 - alpha/2)*target:
-            
+            if i == p-1:
+                last_cluster = True
+                
             #Find and remove the founded subtree
-            
-            sub = BF(remain,first_time,Qu)
+            if remain < 1:
+                break
+            sub = BF(remain,first_time,Qu,last_cluster)
             
             remain = remain - len(sub)
             
             C[i] += sub
             
             first_time = False
-
+            
+            
     return C
 
 
@@ -267,14 +276,14 @@ dist = poisson(1., loc=1).rvs
 random_tree(my_mtg,my_mtg.root, nb_children=dist,nb_vertices=99)
 #my_mtg  = simple_tree(Mtg, Mtg.root,nb_children = 4, nb_vertices=100)
 
-clusters = SFC_BF(my_mtg,10,0.1)
+clusters = SFC_BF(my_mtg,10,0)
 #clusters1 = SFC_FF(my_mtg,10)
 
 for i in range(10):
     print("---------------------------Cluster---------------",i)
     print(clusters[i])
-"""
 
+"""
 A = list(post_order(my_mtg,my_mtg.root))
 
 weight = np.zeros(len(my_mtg))
