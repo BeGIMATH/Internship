02628e185d5a0e5bdd35df55a4bc58495f0ad9f5
bytyqib <Bagatim.Bytyqi@etu.univ-grenoble-alpes.fr>
2020-07-15 17:06:12 +0200

    Some small changes

--- Task_2/Queue.py
+++ Task_2/Queue.py
@@ -273,3 +273,224 @@ def SFC_BF_1(T,c_omponent,p,alpha):
         #first_time = False
     return C
 
+def SFC_BF_MTG(T,p,alpha):
+    
+    C = [[] for i in range(p)]
+    
+    remain = 0
+    
+    weight = np.zeros(len(T))
+    internode_root = T.roots(T.max_scale())
+    
+    a = T.roots(T.max_scale())
+    c_omponent = a[0] 
+    
+    for v in post_order(T,a[0]):
+        weight[v] = 1 + sum(weight[vid] for vid in T.children(v))
+        
+
+    
+    c = int(len(T)/p)
+    color = set()
+    def BF(remain,Q,last_cluster):
+        
+        sub = None
+        
+        if last_cluster:
+            sub = c_omponent
+        
+        else:
+          
+            for vid in post_order(T,c_omponent):#pre_order_filter = lambda v: v not in color):
+                if T.parent(vid) != None:
+                    if weight[vid] <= (1+alpha)*remain  and weight[T.parent(vid)] > remain + alpha:
+                        Q.append(vid)
+                        if weight[vid] > (1-alpha/2)*remain:
+                            break
+               
+            if Q.size() > 0:
+                sub = Q.pop()
+                index = weight[sub]
+                for w in list(ancestors(T,sub)):
+                    weight[w] = weight[w] - index
+               
+                               
+
+        if sub != c_omponent:   
+            sub_tree_found = list(post_order(T,sub))
+        
+            T.remove_tree(sub)
+        elif sub==c_omponent:
+            sub_tree_found = list(post_order2(T,c_omponent,pre_order_filter = lambda v: v not in color))        
+        return sub_tree_found
+        
+    remain = 0
+    last_cluster = False
+    for i in range(p):
+        Qu = Priority_queue(weight)
+        
+        target = c
+        if i == p-1:
+            last_cluster = True
+            
+            
+        sub = BF(target,Qu,last_cluster)
+                
+        
+            
+        C[i] += sub
+            
+        #first_time = False
+    return C
+
+# Iterative Method to traverse the tree in level-order 
+def level_order(T,vtx_id): 
+    # Create an empty queue for level order traversal 
+    queue = [] 
+
+    # Enqueue Root and initialize height 
+    queue.append(vtx_id) 
+
+    while len(queue) > 0: 
+        node = queue.pop(0) 
+        for vid in T.children(node):
+            queue.append(vid)
+        yield node
+
+
+
+def SFC_BF_2(T,c_omponent,p,alpha):
+    
+    C = [[] for i in range(p)]
+    
+    remain = 0
+    
+    weight = np.zeros(len(T))
+    
+    for v in post_order(T,c_omponent):
+        weight[v] = 1 + sum([weight[vid] for vid in T.children(v)])
+    
+    c = int(len(T)/p)
+    color = set()
+    def BF(remain,Q,last_cluster):
+        
+        sub = None
+        
+        if last_cluster:
+            sub = c_omponent
+        
+        else:
+          
+            for vid in level_order(T,c_omponent):#pre_order_filter = lambda v: v not in color):
+                
+                if T.parent(vid) != None:
+                    if weight[vid] <= (1+alpha)*remain  and weight[T.parent(vid)] > remain + alpha:
+                        Q.append(vid)
+                        if weight[vid] > (1-alpha/2)*remain:
+                            break
+               
+            if Q.size() > 0:
+                sub = Q.pop()
+                index = weight[sub]
+                for w in list(ancestors(T,sub)):
+                    weight[w] = weight[w] - index
+               
+                               
+
+        if sub != c_omponent:   
+            sub_tree_found = list(post_order(T,sub))
+        
+            T.remove_tree(sub)
+        elif sub==c_omponent:
+            sub_tree_found = list(post_order2(T,c_omponent,pre_order_filter = lambda v: v not in color))        
+        return sub_tree_found
+        
+    remain = 0
+    last_cluster = False
+    for i in range(p):
+        Qu = Priority_queue(weight)
+        
+        target = c
+        if i == p-1:
+            last_cluster = True
+            
+            
+        sub = BF(target,Qu,last_cluster)
+                
+        
+            
+        C[i] += sub
+            
+        #first_time = False
+    return C
+
+
+def SFC_BF_MTG_1(T,p,alpha):
+    
+    C = [[] for i in range(p)]
+    
+    remain = 0
+    
+    weight = np.zeros(len(T))
+    internode_root = T.roots(T.max_scale())
+    
+    a = T.roots(T.max_scale())
+    c_omponent = a[0] 
+    
+    for v in post_order(T,a[0]):
+        weight[v] = 1 + sum(weight[vid] for vid in T.children(v))
+        
+
+    
+    c = int(len(T)/p)
+    color = set()
+    def BF(remain,Q,last_cluster):
+        
+        sub = None
+        
+        if last_cluster:
+            sub = c_omponent
+        
+        else:
+          
+            for vid in level_order(T,c_omponent):#pre_order_filter = lambda v: v not in color):
+                if T.parent(vid) != None:
+                    if weight[vid] <= (1+alpha)*remain  and weight[T.parent(vid)] > remain + alpha:
+                        Q.append(vid)
+                        if weight[vid] > (1-alpha/2)*remain:
+                            break
+               
+            if Q.size() > 0:
+                sub = Q.pop()
+                index = weight[sub]
+                for w in list(ancestors(T,sub)):
+                    weight[w] = weight[w] - index
+               
+                               
+
+        if sub != c_omponent:   
+            sub_tree_found = list(post_order(T,sub))
+        
+            T.remove_tree(sub)
+        elif sub==c_omponent:
+            sub_tree_found = list(post_order2(T,c_omponent,pre_order_filter = lambda v: v not in color))        
+        return sub_tree_found
+        
+    remain = 0
+    last_cluster = False
+    for i in range(p):
+        Qu = Priority_queue(weight)
+        
+        target = c
+        if i == p-1:
+            last_cluster = True
+            
+            
+        sub = BF(target,Qu,last_cluster)
+                
+        
+            
+        C[i] += sub
+            
+        #first_time = False
+    return C
--- Task_2/clustering.py
+++ Task_2/clustering.py
@@ -18,15 +18,17 @@ from oawidgets.mtg import plot
 from oawidgets.plantgl import PlantGL
 
 
-my_mtg = MTG()
+#my_mtg = MTG()
+my_mtg = MTG('consolidated_mango3d.mtg')
+
 dist = poisson(1., loc=1).rvs
 
 vid = my_mtg.add_component(my_mtg.root)
 
-random_tree(my_mtg,vid,nb_children=dist,nb_vertices=999999)
+#random_tree(my_mtg,vid,nb_children=dist,nb_vertices=999)
 
 
-#my_copy = my_mtg.copy()
+my_copy = my_mtg.copy()
 #random_tree(my_mtg,vid, nb_children=dist,nb_vertices=99)
 
 """
@@ -34,9 +36,9 @@ t5 = timeit.default_timer()
 clusters_1 = SFC_FF(my_mtg,vid,10)
 t6 = timeit.default_timer()
 """
-p = 32
+p = 10
 t3 = timeit.default_timer()
-clusters_2 = SFC_BF(my_mtg,vid,p,0)
+clusters_2 = SFC_BF_MTG(my_mtg,p,0.4)
 t4 = timeit.default_timer()
 
 #print("Time for clustering with the first algorithm based on paper",t2-t1)
@@ -46,6 +48,7 @@ print("Time for clustering with the first algorithm using the queue",t4-t3)
 
 for i in range(p):
     print("------------------------")
-    print("Cluster",i)
-    print(len(clusters_2[i]))
-#plot_clusters(my_,cluster=clusters_2)
+    print("Cluster",i,"with lenght",len(clusters_2[i]))
+    print("with nodes ",clusters_2[i])
+    
+plot_clusters(my_copy,cluster=clusters_2)
