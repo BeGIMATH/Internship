5a3b365418204db67db802e5ae6df5777fad2a68
Begatim Bytyqi <bagatim.bytyqi@etu.univ-grenoble-alpes.fr>
2020-08-22 16:46:04 +0200

    Removed some lines of code

--- Task_1/src/execute.sh
+++ Task_1/src/execute.sh
@@ -5,17 +5,17 @@ n_procs=$(echo $sudoPW|cat - /proc/cpuinfo|grep -m 1 "cpu cores"|awk '{print $ 4
 echo "--------------------- Sequential --------------------"
 ./bench_seq 
 
-echo "----------------- Multithreading--------------------" 
-for n in 4 8 16 32 64 128
-do
-   ./bench_thread $n
-done
+#echo "----------------- Multithreading--------------------" 
+#for n in 4 8 16 32 64 128
+#do
+#   ./bench_thread $n
+#done
 
-echo "-------- Multithreading with multiinterpreters--------"
-for n in 4 8 16 32 64 128
-do
-   ./bench_multi $n
-done
+#echo "-------- Multithreading with multiinterpreters--------"
+#for n in 4 8 16 32 64 128
+#do
+#   ./bench_multi $n
+#done
 
 echo "-----------------Parallel processing-------------------"
 
