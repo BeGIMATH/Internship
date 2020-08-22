8caa50a9c52f81ef0093d9bf29a60569b81910dd
Begatim Bytyqi <bagatim.bytyqi@etu.univ-grenoble-alpes.fr>
2020-08-22 15:17:09 +0200

    Removed some lines of code

--- Task_1/src/bench_seq.cpp
+++ Task_1/src/bench_seq.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char *argv[])
 {
 
-    int list_lengths[3] = {10000, 50000, 100000};
+    int list_lengths[3] = {10000, 20000, 50000};
     int Len = sizeof(list_lengths) / sizeof(list_lengths[0]);
     for (int i = 0; i < Len; i++)
     {
